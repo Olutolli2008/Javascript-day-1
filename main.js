61b993a0f4b143289e73117051dd8bc80b6b0361
Olutolli2008 <167331251+Olutolli2008@users.noreply.github.com>
2024-11-13 17:35:32 +0100

    ...

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log("Orgesi e ka bo projektin me te mire se Fatmiri")
+
+var emri="Orgesi";
+
+document.write(emri);
+
+var mosha=16;
+var bool=true;
+
+ document.write(typeof(emri));//String
+
+ var a = 5;
+ var b = 7;
+
+ console.log(a+b) //12
