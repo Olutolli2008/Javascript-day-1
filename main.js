1ef1062dff2af5f48b4ad7cbfb8928b90f07193d
Olutolli2008 <167331251+Olutolli2008@users.noreply.github.com>
2024-11-18 17:46:43 +0100

    ...

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-console.log("Orgesi e ka bo projektin me te mire se Fatmiri")
-
-var emri="Orgesi";
-
-document.write(emri);
-
 var mosha=16;
 var bool=true;
 
@@ -12,4 +6,52 @@ var bool=true;
  var a = 5;
  var b = 7;
 
- console.log(a+b) //12
+ console.log(a+b); //12
+
+var emri = "Orges";
+var mbiemri = "Lutolli";    
+
+// Shfaqim emrin dhe mbiemrin
+console.log("Emri dhe mbiemri yt eshte: " + emri + " " +  mbiemri);
+
+var d = 6234;
+var e = 35 ;
+// %- modulu
+document.write(d%e);
+
+// ==,===
+
+var t =5;
+var u = 5;
+
+console.log(t== u);
+
+var i =16;
+console.log(i)
+i++;
+console.olog(i)
+
+var o = 4;
+
+console.log(o **2);//fuqia 
+
+console.log(o +=2);
+
+// +=, -=, /=, *=  => i= i * 2
+
+//operatoret e krhahasimit.
+
+var ab = 16;
+var cd = 17;
+
+console.log(ad < cd );
+
+// operatori not
+var evertete =false;
+console.log(evertete)
+
+//operatori logjik || -or,  && -And;
+
+var age =18;
+
+console.log(age >=24 || age <=65);
